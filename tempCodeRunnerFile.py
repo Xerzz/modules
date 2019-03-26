a34960a25aa7c619209b0864c7aa3d38970f9d22
Xerzz <44528455+Xerzz@users.noreply.github.com>
2019-03-26 21:24:48 +0100

    Removing all files in .gitignore

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-logger = logging.getLogger('myapp')
-# hdlr = logging.FileHandler('log.txt')
-# formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-# hdlr.setFormatter(formatter)
-# logger.addHandler(hdlr)
-# logger.setLevel(logging.WARNING)
-
-# logger.error('We have a problem')
-# logger.info('While this is just chatty')
