6fa47620e2fedb55f916a7044353074f2c7eac96
Xerzz <chaper122@gmail.com>
2019-04-04 21:02:29 +0200

    Update open_file.py

--- open_file.py
+++ open_file.py
@@ -1,15 +1,25 @@
-def open_file(file=None):
-    if (file == None):  # sprawdzenie nazwy pliku
-        print('Nie podano nazwy pliku. Domyslne ustawienie: input.txt')
-        file = 'input.txt'
+def open_file(xfile=None, xmode='r'):
+    """
+    Available modes:
+        'r' - read, default
+        'w' - write, WARNING - it overwrites the file if the file exists
+        'a' - append
+        'r+' - read and write, pointer at the beginning of the file
+        'w+' - r and w, WARNING - it overwrites the file if the file exists
+        'a+' - r and w, pointer at the end of the file
+    Returns file with splitines or None if IOError
+    """
+    if (xfile == None):  # check file name
+        print('File name not provided. Default: input.txt')
+        xfile = 'input.txt'
     else:
-        print('Wprowadzona nazwa pliku: ' + file)
+        print('File name: ' + xfile)
 
-    try:  # otwarcie pliku, jesli blad to komunikat
-        with open(str(file), 'r') as fd:
-            file = fd.read().splitlines()
+    try:  # open file
+        with open(str(xfile), xmode) as fd:
+            xfile = fd.read().splitlines()
+        return xfile
     except IOError:
-        print('Blad otwarcia pliku: ' + file)
-        exit(1)
-
-    return file
+        print('Error opening the file: ' + xfile)
+        # exit(1)
+        return None
