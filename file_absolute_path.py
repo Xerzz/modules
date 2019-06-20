83baafbfd2d62fd1a32d7d3d657b33b798119fb9
Xerzz <chaper122@gmail.com>
2019-06-20 19:29:20 +0200

    better function

--- file_absolute_path.py
+++ file_absolute_path.py
@@ -1,10 +1,12 @@
-"""
-:argument - filename (str)
-:returns - absolute path to file in folder of your script
-"""
 import os
 
 
-def file_abspath(filename):
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    return os.path.join(__location__, filename)
+def file_abspath(file):
+    location = os.path.realpath(os.path.dirname(__file__))
+    path = os.path.join(location, file)
+    return path
+
+# script_path = os.path.abspath(__file__)
+# script_dir = os.path.split(script_path)[0]
+# file = "file.txt"
+# abs_file_path = os.path.join(script_dir, file)
